2bc8ed3cc9b207a7b5ab0a3a4d111285704504a4
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-20 20:15:21 +0300

    adding the routing in the app.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Orders from "./Pages/Orders";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="px-4 sm:px-[5vw] sd:px-[7vw] sl:px-[9vw]">
+      <Routes>
+<Route path="/" element={<Home/>} />
+<Route path="/collection" element={<Collection/>} />
+<Route path="/about" element={<About/>} />
+<Route path="/contact" element={<Contact/>} />
+<Route path="/product/:productId" element={<Product/>}/>
+<Route path="/cart" element={<Cart/>}/>
+<Route path="/login" element={<Login/>}/>
+<Route path="/place-order" element={< PlaceOrder/>}/>
+<Route path="/orders" element={<Orders/>}/>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
