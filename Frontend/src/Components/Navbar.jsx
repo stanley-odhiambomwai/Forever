8ba722124d3d448ee3eca7896969fc57d18feb7e
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-21 16:00:29 +0300

    doing updates on pages components

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -7,9 +7,23 @@ const Navbar = () => {
         <img src={assets.logo} className='w-36' alt=''/>
 
 <ul className='hidden sm:flex gap-5 text-sm text-gray-700 '>
-    <NavLink className="flex flex-col items-center gap-1">
-
+    <NavLink to='/' className="flex flex-col items-center gap-1">
+<p>HOME</p>
+<hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
     </NavLink>
+    <NavLink to='/collection' className="flex flex-col items-center gap-1">
+<p>COLLECTION</p>
+<hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+    </NavLink>
+    <NavLink to='/about' className="flex flex-col items-center gap-1">
+<p>ABOUT</p>
+<hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+    </NavLink>
+    <NavLink to='/contact' className="flex flex-col items-center gap-1">
+<p>CONTACT</p>
+<hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+    </NavLink>
+
 </ul>
     </div>
   )
--- Frontend/src/Pages/PlaceOrder.jsx
+++ Frontend/src/Pages/PlaceOrder.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <div></div>
   )
 }
 
