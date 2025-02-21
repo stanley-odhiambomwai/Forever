6b8f0ded2c6830b6e918b4c31739fe79e0d14042
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-21 14:13:16 +0300

    updating the navbar and main.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {assets} from "../assets/assets"
+import { NavLink } from 'react-router'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' flex items-center justify-between py-5 font-medium'>
+        <img src={assets.logo} className='w-36' alt=''/>
+
+<ul className='hidden sm:flex gap-5 text-sm text-gray-700 '>
+    <NavLink className="flex flex-col items-center gap-1">
+
+    </NavLink>
+</ul>
+    </div>
   )
 }
 
