5929bfff9c97b199571868f92b4a4938e8fd1120
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-11 11:07:42 +0300

    doing styling on the hero section and adding some detail futures

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
-
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+      {/* Hero left side */}
+      <div className="w-full sm:w-1/2 flex items-center justify-center py-18 sm:py-0">
+        <div className="text-[#414141]">
+          <div className="flex items-center gap-2">
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
+          </div>
+          <h1 className="text-3xl sm:py-3">Latest arrival</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
