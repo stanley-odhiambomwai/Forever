effbb82f7e985525e8740307a1a2cebc3c79843c
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-11 11:23:13 +0300

    adding the latest arrival and shop now, along side styling them

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -10,7 +10,13 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">Latest arrival</h1>
+          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+            Latest Arrival
+          </h1>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+          </div>
         </div>
       </div>
     </div>
